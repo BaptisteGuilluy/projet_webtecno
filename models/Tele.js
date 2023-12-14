b8f7a0359af7b0c90bca66750364722db85eec50
guilluy <baptisteguilluy0728@gmail.com>
2023-12-14 13:04:34 +0100

    création github

---
+++ models/Tele.js
@@ -0,0 +1,9 @@
+import Model from './model.js';
+
+export default class Tele extends Model {
+
+  static table = "user.television";
+  static primary = ["id"];
+
+  // Additional methods or properties specific to the 'schema.tasks' table can be added here.
+}
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import Tele from './models/Tele.js';
+import express from 'express';
+
 const app = express();
 app.use(express.static("public"));
 
@@ -8,19 +10,36 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.static('public'));
 
-app.post('/', (request,response)=>{
+app.get("/", async function (req, res) {
+  const televisions_liste = await Tele.loadMany({pascasse:0});
+  const possedé = await Tele.loadMany({pascasse : 1})
+  res.render('home.ejs', { televisions_liste,possedé });
+});
 
-    if (request.body.something != null){
-        //do something
-    }
-})
+app.post("/add", async function (req, res) {
+    const televisions = new Tele();
+    televisions.marque = req.body.marque;
+    televisions.prix = parseInt(req.body.prix);
+    televisions.taille = parseInt(req.body.taille);
+    televisions.achetée = 0;
+    await televisions.save();
+    res.redirect('/');
+});
+app.post("/buy", async function (req, res) {
+  const televisions = await Tele.load({id : req.body.id_tele})
+  televisions.update({pascasse : 1});
+  televisions.update({casse : 0})
+  await televisions.save();
+  res.redirect('/');
+});
 
-
-app.get('/',(request,response)=>{
-    if (request.query.something != null){
-        //do something
-    }
-})
+app.post("/broke", async function (req, res) {
+  const televisions = await Tele.load({id : req.body.id_tele})
+  televisions.update({casse : 1});
+  televisions.update({raisoncasse : req.body.raisoncasse})
+  await televisions.save();
+  res.redirect('/');
+});
 
 app.listen(3000, function(){
     console.log("Server ok");
